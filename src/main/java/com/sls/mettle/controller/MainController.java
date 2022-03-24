43eef0edce87c06545c55a4925d85405ced73e8c
Andrey Minin <ewkjf@mail.ru>
2022-03-24 18:01:04 +0400

    added logging with log4j2

--- src/main/java/com/sls/mettle/controller/MainController.java
+++ src/main/java/com/sls/mettle/controller/MainController.java
@@ -5,6 +5,7 @@ import com.sls.mettle.model.Item;
 import com.sls.mettle.model.Views;
 import com.sls.mettle.service.ItemsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.management.openmbean.KeyAlreadyExistsException;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 public class MainController {
@@ -31,22 +34,42 @@ public class MainController {
     }
 
     @PostMapping("/item")
-    public Item addItem(@JsonView(Views.Public.class) @RequestBody Item item) {
-        return itemsService.addItem(item);
+    public ResponseEntity<Item> addItem(@JsonView(Views.Public.class) @RequestBody Item item) {
+        try {
+            return ResponseEntity.ok()
+                    .body(itemsService.addItem(item));
+        } catch (KeyAlreadyExistsException ex) {
+            return ResponseEntity.badRequest().body(null);
+        }
     }
 
     @PutMapping("/item")
-    public Item editItem(@JsonView(Views.Public.class) @RequestBody Item item) {
-        return itemsService.updateItem(item);
+    public ResponseEntity<Item> editItem(@JsonView(Views.Public.class) @RequestBody Item item) {
+        try {
+            return ResponseEntity.ok()
+                    .body(itemsService.updateItem(item));
+        } catch (NoSuchElementException ex) {
+            return ResponseEntity.badRequest().body(null);
+        }
     }
 
     @DeleteMapping("/item/{uuid}")
-    public Item deleteItem(@PathVariable String uuid) {
-        return itemsService.deleteItem(uuid);
+    public ResponseEntity<Item> deleteItem(@PathVariable String uuid) {
+        try {
+            return ResponseEntity.ok()
+                    .body(itemsService.deleteItem(uuid));
+        } catch (NoSuchElementException ex) {
+            return ResponseEntity.badRequest().body(null);
+        }
     }
 
     @GetMapping("/item/{uuid}")
-    public Item getItem(@PathVariable String uuid) {
-        return itemsService.getItem(uuid);
+    public ResponseEntity<Item> getItem(@PathVariable String uuid) {
+        try {
+            return ResponseEntity.ok()
+                    .body(itemsService.getItem(uuid));
+        } catch (NoSuchElementException ex) {
+            return ResponseEntity.badRequest().body(null);
+        }
     }
 }
--- src/main/java/com/sls/mettle/service/PsqlItemsService.java
+++ src/main/java/com/sls/mettle/service/PsqlItemsService.java
@@ -2,6 +2,8 @@ package com.sls.mettle.service;
 
 import com.sls.mettle.model.Item;
 import com.sls.mettle.repository.ItemsRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,10 @@ import java.util.UUID;
 
 @Service
 public class PsqlItemsService implements ItemsService {
+
+    private static final Logger log = LogManager.getLogger(ItemsService.class);
+    private static final String ELEMENT_ALREADY_EXISTS = "The item with such ID already exists.";
+    private static final String NO_ITEM_WITH_ID = "There is no item with such ID";
 
     private final ItemsRepository itemsRepository;
 
@@ -30,51 +36,58 @@ public class PsqlItemsService implements ItemsService {
 
     @Override
     public Item addItem(Item item) {
-        Optional<Item> optionalItem = this.itemsRepository.findById(item.getId());
-        if (optionalItem.isPresent()) {
-            throw new KeyAlreadyExistsException("The item with such ID already exists.");
-        }
+        tryFindElementById(item.getId(), false);
         item.setUpdatedAt(Timestamp.from(Instant.now()));
         item.setCreatedAt(Timestamp.from(Instant.now()));
-        return itemsRepository.save(item);
+        Item save = itemsRepository.save(item);
+        log.info("Successfully saved item into database");
+        return save;
     }
 
     @Override
     public Item updateItem(Item item) {
-        Optional<Item> optionalItem = this.itemsRepository.findById(item.getId());
-        if (optionalItem.isEmpty()) {
-            throw new NoSuchElementException("There is no item with such ID");
-        }
-        Item oldItem = optionalItem.get();
+        Item oldItem = tryFindElementById(item.getId(), true);
         oldItem.setName(item.getName());
         oldItem.setDescription(item.getDescription());
         oldItem.setCost(item.getCost());
         oldItem.setType(item.getType().toString());
         oldItem.setUpdatedAt(Timestamp.from(Instant.now()));
         this.itemsRepository.save(oldItem);
+        log.info("Successfully updated item");
         return oldItem;
     }
 
     @Override
     public Item deleteItem(String uuid) {
-        Optional<Item> optionalItem = this.itemsRepository.findById(UUID.fromString(uuid));
-        if (optionalItem.isEmpty()) {
-            throw new NoSuchElementException("There is no item with such ID");
-        }
-        Item oldItem = optionalItem.get();
+        Item oldItem = tryFindElementById(uuid, true);
         // it should be added here, but then I could not understand, why do we need a deleted_at property...
 //        this.itemsRepository.deleteById(oldItem.getId());
         oldItem.setDeletedAt(Timestamp.from(Instant.now()));
         this.itemsRepository.save(oldItem);
+        log.info("Successfully deleted item");
         return oldItem;
     }
 
     @Override
     public Item getItem(String uuid) {
-        Optional<Item> optionalItem = this.itemsRepository.findById(UUID.fromString(uuid));
-        if (optionalItem.isEmpty()) {
-            throw new NoSuchElementException("There is no item with such ID");
+        return tryFindElementById(uuid, true);
+    }
+
+    private Item tryFindElementById(String uuid, boolean isNeededToExist) {
+        return tryFindElementById(UUID.fromString(uuid), isNeededToExist);
+    }
+
+    private Item tryFindElementById(UUID uuid, boolean isNeededToExist) {
+        Optional<Item> optionalItem = this.itemsRepository.findById(uuid);
+        if (optionalItem.isPresent() ^ isNeededToExist) {
+            if (isNeededToExist) {
+                log.error(NO_ITEM_WITH_ID);
+                throw new NoSuchElementException(NO_ITEM_WITH_ID);
+            } else {
+                log.error(ELEMENT_ALREADY_EXISTS);
+                throw new KeyAlreadyExistsException(ELEMENT_ALREADY_EXISTS);
+            }
         }
-        return optionalItem.get();
+        return optionalItem.orElse(null);
     }
 }
